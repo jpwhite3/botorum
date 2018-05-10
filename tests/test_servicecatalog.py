93f7a8ec9670222215eef0c5384e653a4470d983
JP White <jpwhite3@gmail.com>
2018-05-09 21:25:04 -0400

    fixing tests

--- tests/test_servicecatalog.py
+++ tests/test_servicecatalog.py
@@ -14,10 +14,6 @@ def test_classes_exist():
     assert inspect.isclass(Portfolio)
 
 
-def test_properties_exist():
-    assert inspect.isfunction(Portfolio.create)
-
-
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6")
 def test_methods_exist():
     # TODO: Figure out why ismethod test fails
