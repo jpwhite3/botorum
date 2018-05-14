18659f244ae9f54b0f3743cca10dd5f5f46f7faa
JP White <jpwhite3@gmail.com>
2018-05-13 21:08:16 -0400

    daily changes

--- botorum/servicecatalog/portfolio.py
+++ botorum/servicecatalog/portfolio.py
@@ -4,13 +4,22 @@ import boto3
 class Portfolio(object):
 
     client = boto3.client('servicecatalog')
-    config = dict()
+    language = 'en'
 
     @classmethod
-    def list(cls, AcceptLanguage='en', MaxItems=1000, PageSize=20):
+    def list(cls, MaxItems=1000, PageSize=20):
+        """
+        Generator function that lists all portfolios in the account.
+        Paginates 20 items per page by default, the max as of this writing.
+
+        Keyword Arguments:
+            MaxItems {int} -- The total # of items to return (default: {1000})
+            PageSize {int} -- The size of each page (default: {20})
+        """
+
         paginator = cls.client.get_paginator('list_portfolios')
         response_iterator = paginator.paginate(
-            AcceptLanguage=AcceptLanguage,
+            AcceptLanguage=cls.language,
             PaginationConfig={'MaxItems': MaxItems, 'PageSize': PageSize}
         )
         for response in response_iterator:
@@ -19,31 +28,45 @@ class Portfolio(object):
 
     def __init__(self, **kwargs):
         portfolio_id = kwargs.pop('portfolio_id', None)
-        self.config['default_language'] = kwargs.get('AcceptLanguage', 'en')
         if portfolio_id:
             self.__load(portfolio_id)
         else:
             self.__create(**kwargs)
 
-    def __setattrs__(self, **attrs):
+    def __set_attrs(self, **attrs):
         for attr, value in attrs.items():
             setattr(self, attr, value)
 
+    def __set_tags(self, tag_list):
+        if tag_list:
+            tag_dict = {x['Key']: x['Value'] for x in tag_list}
+            setattr(self, 'Tags', tag_dict)
+
+    def __set_tag_options(self, tag_option_list):
+        if tag_option_list:
+            setattr(self, 'TagOptions', tag_option_list)
+
+    def __populate(self, response):
+        attrs = response.get('PortfolioDetail', {})
+        tags = response.get('Tags', [])
+        tag_options = response.get('TagOptions', [])
+        self.__set_attrs(**attrs)
+        self.__set_tags(tags)
+        self.__set_tag_options(tag_options)
+
     def __create(self, **kwargs):
         response = self.client.create_portfolio(**kwargs)
-        portfolio_attrs = response.get('PortfolioDetail', {})
-        self.__setattrs__(**portfolio_attrs)
+        self.__populate(response)
 
     def __load(self, portfolio_id):
         response = self.client.describe_portfolio(
-            AcceptLanguage=self.config['default_language'],
+            AcceptLanguage=self.language,
             Id=portfolio_id
         )
-        portfolio_attrs = response.get('PortfolioDetail', {})
-        self.__setattrs__(**portfolio_attrs)
+        self.__populate(response)
 
     def delete(self):
         return self.client.delete_portfolio(
-            AcceptLanguage=self.config['default_language'],
+            AcceptLanguage=self.language,
             Id=self.Id
         )
--- tests/test_servicecatalog.py
+++ tests/test_servicecatalog.py
@@ -24,15 +24,29 @@ def portfolio_config():
     }
 
 
+@pytest.fixture(scope="module")
+def tag_options():
+    return [
+        {
+            'Key': 'string',
+            'Value': 'string',
+            'Active': True | False,
+            'Id': 'string'
+        },
+    ]
+
+
 def test_classes_exist():
     assert inspect.isclass(Portfolio)
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6")
 def test_001_methods_exist():
-    # TODO: Figure out why ismethod test fails
     assert inspect.isfunction(Portfolio.__init__)
-    assert inspect.isfunction(Portfolio.__setattrs__)
+    assert inspect.isfunction(Portfolio._Portfolio__set_attrs)
+    assert inspect.isfunction(Portfolio._Portfolio__set_tags)
+    assert inspect.isfunction(Portfolio._Portfolio__set_tag_options)
+    assert inspect.isfunction(Portfolio._Portfolio__populate)
     assert inspect.isfunction(Portfolio._Portfolio__create)
     assert inspect.isfunction(Portfolio._Portfolio__load)
     assert inspect.isgeneratorfunction(Portfolio.list)
@@ -55,6 +69,9 @@ def test_003_instance_creation_and_deletion(portfolio_config):
     assert test_portfolio.Description == portfolio_config['Description']
     assert test_portfolio.ProviderName == portfolio_config['ProviderName']
 
+    tag_list = portfolio_config['Tags']
+    assert test_portfolio.Tags == {x['Key']: x['Value'] for x in tag_list}
+
     assert test_portfolio.delete()
     assert len([item for item in Portfolio.list()]) == 0
 
@@ -65,15 +82,33 @@ def test_004_instance_load(portfolio_config):
     test_portfolio = Portfolio(**portfolio_config)
     arbitrary_portfolio = Portfolio(portfolio_id=test_portfolio.Id)
     assert len([item for item in Portfolio.list()]) == 1
-
     assert test_portfolio is not arbitrary_portfolio
     assert test_portfolio.Id == arbitrary_portfolio.Id
-    assert test_portfolio.Id
-    assert test_portfolio.ARN
-    assert test_portfolio.CreatedTime
-    assert test_portfolio.DisplayName == portfolio_config['DisplayName']
-    assert test_portfolio.Description == portfolio_config['Description']
-    assert test_portfolio.ProviderName == portfolio_config['ProviderName']
+
+    assert arbitrary_portfolio.Id
+    assert arbitrary_portfolio.ARN
+    assert arbitrary_portfolio.CreatedTime
+    assert arbitrary_portfolio.DisplayName == portfolio_config['DisplayName']
+    assert arbitrary_portfolio.Description == portfolio_config['Description']
+    assert arbitrary_portfolio.ProviderName == portfolio_config['ProviderName']
+
+    tag_list = portfolio_config['Tags']
+    assert arbitrary_portfolio.Tags == {x['Key']: x['Value'] for x in tag_list}
+
+    arbitrary_portfolio.delete()
+    assert len([item for item in Portfolio.list()]) == 0
+
+
+def test_005_tag_option_loading(portfolio_config, tag_options):
+    assert len([item for item in Portfolio.list()]) == 0
+
+    test_portfolio = Portfolio(**portfolio_config)
+    test_portfolio._Portfolio__set_tag_options(tag_options)
+
+    assert len(test_portfolio.TagOptions) == 1
+    actual_tag_options = test_portfolio.TagOptions[0]
+    expected_tag_options = tag_options[0]
+    assert actual_tag_options == expected_tag_options
 
     test_portfolio.delete()
     assert len([item for item in Portfolio.list()]) == 0
